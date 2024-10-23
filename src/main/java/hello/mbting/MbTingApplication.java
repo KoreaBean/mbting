d50623c44695b651f468ec03aebf8edee9883453
KoreaBean <155369309+KoreaBean@users.noreply.github.com>
2024-10-23 12:32:02 +0900

    git init

---
+++ src/main/java/hello/mbting/MbTingApplication.java
@@ -0,0 +1,13 @@
+package hello.mbting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbTingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbTingApplication.class, args);
+    }
+
+}
